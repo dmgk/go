31c81bb9cf7c66874916aeed65a4b20f0adeb551
MikaelUrankar <mikael.urankar@gmail.com>
2022-06-10 11:26:34 +0200

    Add OpenBSD and NetBSD, they may port go/riscv64 on their OS

--- src/cmd/vendor/golang.org/x/sys/unix/asm_bsd_riscv64.s
+++ src/cmd/vendor/golang.org/x/sys/unix/asm_bsd_riscv64.s
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (freebsd) && gc
-// +build freebsd
+//go:build (freebsd || netbsd || openbsd) && gc
+// +build freebsd netbsd openbsd
 // +build gc
 
 #include "textflag.h"
